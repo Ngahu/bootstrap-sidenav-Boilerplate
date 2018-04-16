ea8a0c765196894c030a12875a0bd7dc6741fefc
Ngahu <jamaalaraheem@gmail.com>
2018-04-16 15:16:51 +0300

    creating of the body content containing  example content for making the page scrollable

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -23,7 +23,7 @@ export  class Sidebar  extends Component {
                     <ul className="list-unstyled components">
 
                         <li className="active">
-                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
+                        <a href="#homeSubmenu" >
                             <i className="glyphicon glyphicon-home"></i>
                             Home
                         </a>
@@ -35,6 +35,19 @@ export  class Sidebar  extends Component {
                         about
                     </a>
                 </li>
+                <li>
+                        <a href="#">
+                            <i className="glyphicon glyphicon-paperclip"></i>
+                            FAQ
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i className="glyphicon glyphicon-link"></i>
+                            Portfolio
+                        </a>
+                    </li>
 
                 <li>
                     <a href="#">
@@ -49,14 +62,54 @@ export  class Sidebar  extends Component {
 
                 </nav>
 
-                <div id="content">
-                
-                <button type="button" id="sidebarCollapse" className="btn btn-info navbar-btn">
-                <i className="glyphicon glyphicon-align-left"></i>
-                Toggle Sidebar
-                </button>
+                  {/* Page Content Holder  */}
+                  <div id="content">
 
-                </div>
+                        <nav className="navbar navbar-default">
+                            <div className="container-fluid">
+
+                                <div className="navbar-header">
+                                    <button type="button" id="sidebarCollapse" className="btn btn-info navbar-btn">
+                                        <i className="glyphicon glyphicon-align-left"></i>
+                                        <span>Toggle Sidebar</span>
+                                    </button>
+                                </div>
+
+                                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                    <ul className="nav navbar-nav navbar-right">
+                                        <li><a href="#">Page</a></li>
+                                        <li><a href="#">Page</a></li>
+                                        <li><a href="#">Page</a></li>
+                                        <li><a href="#">Page</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+
+                        <div>
+
+                        <h2>Collapsible Sidebar Using Bootstrap 3</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                        <div className="line"></div>
+
+                        <h2>Lorem Ipsum Dolor</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                        <div className="line"></div>
+
+                        <h2>Lorem Ipsum Dolor</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                        <div className="line"></div>
+
+                        <h3>Lorem Ipsum Dolor</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+             
+             </div>
+                 {/* Page Content Holder  */}
                 
 
 
